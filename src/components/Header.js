fa606875dd037fe726e4182416a2c6d74458ab99
Sheetal Thube <sheetalthube04@gmail.com>
2024-10-21 18:26:22 +0530

    updaed

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import React from 'react'
 export const Header = () => {
   return (
     <div>
+         <h2 className='heading'>Budget Buddy</h2>
          <h2>Expence Tracker</h2>
     </div>
   )
